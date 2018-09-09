98091e37cae9e8d0bedaecabdea8547e633b2b2d
jimmymaise <the@duyet.com>
2018-09-09 15:21:06 +0700

    refactor ES

--- server/models/user-topic.js
+++ server/models/user-topic.js
@@ -1,83 +1,16 @@
 'use strict';
-const es = require('../elasticsearch/connection').es
 const Joi = require('joi');
-const MongoModels = require('mongo-models');
-const userTopicSchema = require('../elasticsearch/mapping/user-topic').userTopic
-
-class UserTopic extends MongoModels {
+const ESMongoModels = require('./es-mongo-model');
+const esSchema = require('../elasticsearch/mapping/user-topic').userTopic
 
 
-  //Override these function to inject update ES
-
-  static async findByIdAndUpdate() {
-    let data = await super.findByIdAndUpdate.apply(this, arguments)
-    await this.upsertES(arguments[0])
-    return data
-  }
-
-  static async findByIdAndDelete() {
-    let data = await super.findByIdAndDelete.apply(this, arguments)
-    await this.deleteES(arguments[0])
-    return data
-  }
-
-  static async insertOne() {
-    let data = await super.insertOne().apply(this, arguments)
-    await this.upsertES(data['_id'])
-    return data
-  }
-
-  static async upsertES(_id) {
-    await es.initIndex(this.collectionName, userTopicSchema)
-    let data = await this.findById(_id)
-    delete data['_id'];
-    await es.update({
-      index: this.collectionName, type: '_doc', id: _id.toString(), body: {doc: data}, doc_as_upsert: true
-    })
-  }
-
-  static async deleteES(_id) {
-    await es.initIndex(this.collectionName, userTopicSchema)
-    es.delete({
-      index: this.collectionName, type: '_doc', id: _id.toString()
-    })
-  }
-
-  static async syncDataES(query = {}) {
-    let page = 1
-    while (page) {
-      let resp = await this.pagedFind(query, page, 3)
-      let data = resp.data
-      for (let i = 0; i < data.length; i++) {
-        await this.upsertES(data[i]._id)
-      }
-      console.log(page)
-      if (!resp.pages.hasNext) {
-        break
-      }
-      page = resp.pages.next
-
-
-    }
-
-
-  }
-
-  static async search(body) {
-    return await es.search({
-      index: this.collectionName,
-      body: body
-    });
-
-
-  }
-
+class UserTopic extends ESMongoModels {
 
 };
 
-UserTopic['buider'] = es.builder
-UserTopic.collectionName = 'user_topic';
 
+UserTopic.collectionName = 'user_topic';
+UserTopic.esSchema = esSchema;
 
 UserTopic.schema = Joi.object().keys({
   _id: Joi.object(),
