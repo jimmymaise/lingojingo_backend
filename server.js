e9a826ff48ceb0dbe0a43106fece18f4c6e5f452
jimmymaise <the@duyet.com>
2018-09-17 14:09:56 +0700

    add auth

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 'use strict';
 
 const composer = require('./index');
-const { ApolloServer,makeExecutableSchema, gql } = require('apollo-server-hapi');
+const {ApolloServer, makeExecutableSchema, gql} = require('apollo-server-hapi');
 const Hapi = require('hapi');
 const typeDefs = require('./server/graphql/schema.js').typeDefs
 const resolvers = require('./server/graphql/schema.js').resolvers
 
 
+'use strict';
+const FIRE_BASE_CONFIG = require('./server/data/firebase_config')
+const Boom = require('boom');
+const {graphqlHapi, graphiqlHapi} = require('apollo-server-hapi');
+const firebaseAdmin = require('firebase-admin');
+
+// const Config = require('../config');
+const serviceAccount = FIRE_BASE_CONFIG.service[process.env.NODE_ENV];
+const graphqlSchema = require('./server/graphql/schema');
+
+firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert(serviceAccount),
+  databaseURL: FIRE_BASE_CONFIG.databaseURL[process.env.NODE_ENV]
+});
+// internals.applyStrategy = async function (server) {
+//   server.auth.strategy('firebase', 'firebase', {
+//     instance: firebaseAdmin
+//   });
 
 const Config = require('./config');
-
 
 
 const schema = makeExecutableSchema({
@@ -18,17 +35,21 @@ const schema = makeExecutableSchema({
 });
 
 
-
-
 const StartServer = async () => {
   try {
-    const server = new ApolloServer({ schema, context: async ({ request, h }) => {
-        return {};
-      }});
+    const server = new ApolloServer({
+      schema, // You'll need to pass the request object and optionally the repsponse toolkit.
+      context: (request, h) => ({request, h}),
+    });
     const app = await composer();
-
+    app.auth.strategy('firebase', 'firebase', {
+      instance: firebaseAdmin
+    })
     await server.applyMiddleware({
       app,
+      route: {
+        auth: 'firebase'
+      }
     });
     await server.installSubscriptionHandlers(app.listener);
     await app.start();
