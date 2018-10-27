a48cf5267bce1b5e4f7ea56a5b317b9e958b70b3
jimmymaise <the@duyet.com>
2018-10-27 23:58:15 +0700

    fix ES local

--- server/api/index.js
+++ server/api/index.js
@@ -3,8 +3,7 @@
 const internals = {};
 const envAuth = process.env.NODE_ENV === 'production' ? 'firebase' : null
 const Wreck = require('wreck');
-// let esAddr=`${process.env.ES_HOST }:9200`
-// let esAddr = (process.env.ES_HOST)?`${process.env.ES_HOST }:9200`:`https://stag-api.vomemo.com/es`
+let esAddr = (process.env.ES_HOST)?`${process.env.ES_HOST }:9200`:`https://stag-api.vomemo.com/es`
 
 internals.applyRoutes = function (server) {
   server.route({
@@ -54,7 +53,7 @@ internals.applyRoutes = function (server) {
       proxy: {
         mapUri: function (request) {
           return {
-            uri: `http://${process.env.ES_HOST || '13.76.130.203'}:9200/${request.params.p}`
+            uri: `${esAddr}/${request.params.p}`
           };
         },
         onResponse: function (err, res, request, h, settings, ttl) {
