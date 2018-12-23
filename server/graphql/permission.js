92db0f01e0e7ae9955b467ba6a0c9801dcb34eee
jimmymaise <the@duyet.com>
2018-12-24 01:32:24 +0700

    add permission for CRUD

--- server/graphql/permission.js
+++ server/graphql/permission.js
@@ -1,12 +1,32 @@
 const { rule, shield, and, or, not }  = require('graphql-shield')
 
 const isAdmin = rule()(async (parent, args, ctx, info) => {
-    return true
+    let credentials = ctx.request.auth.credentials
+    let claims = credentials.claims
+    if (!claims){
+        setClaimToFireBase = require ('../utils/general').setClaimToFireBase
+        claims = await setClaimToFireBase(credentials.user_id)
+    }
+    if (claims.groups.includes('ADMIN')) {
+        return true
+    }
+    return false
 })
-console.log(isAdmin)
+
 const permissions = shield({
     Mutation: {
         createOneCard: isAdmin,
+        deleteOneCard: isAdmin,
+        updateOneCard: isAdmin,
+
+        createOneDeck: isAdmin,
+        deleteOneDeck: isAdmin,
+        updateOneDeck: isAdmin,
+
+        createOneTopic: isAdmin,
+        deleteOneTopic: isAdmin,
+        updateOneTopic: isAdmin,
+
     },
 })
 
