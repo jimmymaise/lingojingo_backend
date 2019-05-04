00f02ffa12054b0e6ac9f10de743d9235cb11816
jimmymaise <the@duyet.com>
2019-05-04 14:56:01 +0700

    update redis url

--- server/redis/connection.js
+++ server/redis/connection.js
@@ -1,7 +1,7 @@
 let redis = require("async-redis")
 let client = redis.createClient(
   {
-    url: process.env.REDIS_URL ? process.env.REDIS_URL : 'redis://:Lingo@123Jingo@localhost:6379/0',
+    url: process.env.REDIS_URL ? process.env.REDIS_URL : 'redis://:Lingo123Jingo@localhost:6379/0',
   }
 );
 
